08d84dff9707704fd51f6142a0c5481a766ee0b1
Kahyberth <kahyberth.gonzalez@correounivalle.edu.co>
2023-09-04 18:48:37 -0500

    refactor Board

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -3,6 +3,8 @@ import board.Board;
 public class Main {
     public static void main(String[] args) {
         Board sudokuBoard = new Board(9);
-        sudokuBoard.fillBoard();
+        int estado = sudokuBoard.fillBoard();
+        System.out.println(estado);
+
     }
 }
--- src/board/Board.java
+++ src/board/Board.java
@@ -1,32 +1,50 @@
 package board;
 
+import java.sql.Array;
+
 public class Board {
 
-    private int coord_x = 0;
-    private int coord_y = 0;
-
     private int quantity;
-
+    private final int [][]sudoku_board = new int[quantity][quantity];
     public Board(int quantity) {
         this.quantity = quantity;
         System.out.println("Sudoku Iniciado!");
     }
 
-
-    public int board(int quantity) {
-        int [][]sudoku_board = new int[quantity][quantity];
-
-        return 0;
-    }
-
-    public void fillBoard(){
-        int random = (int)(Math.random()*10 + 4);
-        int [] current_row = new int[quantity];
-        int [] current_column = new int[quantity];
-        System.out.println("Random: " + random);
-        for(int i = 1; i < random; i++) {
-            System.out.println((int)(Math.random()*i) + 1);
+    public int[][] showBoard () {
+        for(int i = 0; i < sudoku_board.length; i++) {
+            for (int j = 0; j < sudoku_board.length; j++) {
+                System.out.println(sudoku_board[i][j] + "\n");
+            }
         }
+        return this.sudoku_board;
+    }
+    //TODO: The conditionals of the loops need to be corrected and retested.
+    public int fillBoard() {
+        int random_value = (int)(Math.random()*8 + 1);
+        int index_column = 0, index_row = 0;
+        int [] current_row = new int [quantity];
+        for (int i = 0; i <= this.sudoku_board.length; i++) {
+           if ( current_row.length > 0 ) {
+               this.sudoku_board[index_row][index_column] = current_row[index_column];
+           }
+            for (int j = 0; j < this.sudoku_board.length; j++) {
+                random_value = (int)(Math.random()*8 + 1); //Generates random numbers
+                if ( current_row[j] != random_value ) {
+                    current_row[index_column] = random_value;
+                    index_column ++;
+                    break;
+                }
+            }
+            //Reinicia el valor del current_row, index_column y incrementa la fila
+            if ( current_row.length > 8 ) {
+                index_row++;
+                index_column = 0;
+                current_row = new int [quantity];
+            }
+        }
+
+        return this.sudoku_board.length;
     }
 
 
