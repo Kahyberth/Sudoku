ba56fb04495077ad04b0ed0bfe48bbba1161c48b
Kahyberth <kahyberth.gonzalez@correounivalle.edu.co>
2023-09-05 18:05:32 -0500

    refactor Board

--- src/board/Board.java
+++ src/board/Board.java
@@ -1,52 +1,85 @@
 package board;
 
-import java.sql.Array;
+import java.util.Random;
 
 public class Board {
 
     private int quantity;
-    private final int [][]sudoku_board = new int[quantity][quantity];
+    private int[][] sudoku_board = new int[this.quantity][this.quantity];
+
     public Board(int quantity) {
         this.quantity = quantity;
+        this.sudoku_board = new int[quantity][quantity];
         System.out.println("Sudoku Iniciado!");
+
     }
 
-    public int[][] showBoard () {
-        for(int i = 0; i < sudoku_board.length; i++) {
-            for (int j = 0; j < sudoku_board.length; j++) {
-                System.out.println(sudoku_board[i][j] + "\n");
-            }
-        }
-        return this.sudoku_board;
-    }
-    //TODO: The conditionals of the loops need to be corrected and retested.
-    public int fillBoard() {
-        int random_value = (int)(Math.random()*8 + 1);
-        int index_column = 0, index_row = 0;
-        int [] current_row = new int [quantity];
-        for (int i = 0; i <= this.sudoku_board.length; i++) {
-           if ( current_row.length > 0 ) {
-               this.sudoku_board[index_row][index_column] = current_row[index_column];
-           }
-            for (int j = 0; j < this.sudoku_board.length; j++) {
-                random_value = (int)(Math.random()*8 + 1); //Generates random numbers
-                if ( current_row[j] != random_value ) {
-                    current_row[index_column] = random_value;
-                    index_column ++;
-                    break;
-                }
-            }
-            //Reinicia el valor del current_row, index_column y incrementa la fila
-            if ( current_row.length > 8 ) {
-                index_row++;
-                index_column = 0;
-                current_row = new int [quantity];
-            }
-        }
 
+    public int showBoard() {
         return this.sudoku_board.length;
     }
 
+    //TODO: The conditionals of the loops need to be corrected and retested.
 
+    public int[][] generateSudoku(int quantity) {
+        int[][] sudoku_board = new int[9][9];
+        generateSudokuRecursive(sudoku_board, 0, 0);
+        return sudoku_board;
+    }
+
+    private boolean generateSudokuRecursive(int[][] board, int row, int col) {
+        if (row == 9) {
+            return true; // Hemos completado el Sudoku
+        }
+
+        int nextRow = (col == 8) ? row + 1 : row;
+        int nextCol = (col == 8) ? 0 : col + 1;
+
+        int[] possibleValues = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        shuffleArray(possibleValues);
+
+        for (int value : possibleValues) {
+            if (isValidMove(board, row, col, value)) {
+                board[row][col] = value;
+                if (generateSudokuRecursive(board, nextRow, nextCol)) {
+                    return true;
+                }
+                board[row][col] = 0; // Deshacer la asignación si no lleva a una solución
+            }
+        }
+
+        return false;
+    }
+
+    private boolean isValidMove(int[][] board, int row, int col, int value) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == value || board[i][col] == value) {
+                return false; // Valor ya en fila o columna
+            }
+        }
+
+        int startRow = row - row % 3;
+        int startCol = col - col % 3;
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+                if (board[i][j] == value) {
+                    return false; // Valor ya en el cuadro 3x3
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private void shuffleArray(int[] arr) {
+        Random rand = new Random();
+        for (int i = arr.length - 1; i > 0; i--) {
+            int index = rand.nextInt(i + 1);
+            int temp = arr[index];
+            arr[index] = arr[i];
+            arr[i] = temp;
+        }
+    }
 
 }
+
